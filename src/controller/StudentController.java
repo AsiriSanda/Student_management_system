015c8f67226658afcbd5660c7bec3f3c58563c7f
AsiriSanda <sadakelum1234@gmail.com>
2022-09-20 09:50:53 +0530

    Student controller method created

--- src/controller/StudentController.java
+++ src/controller/StudentController.java
@@ -1,12 +1,24 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
 public class StudentController {
     public TextField txtId;
     public TextField txtNIC;
     public TextField txtAddress;
+    public TextField txtEmail;
+    public TextField txtName;
+    public TextField txtContact;
+    public TableView tblStudent;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colEmail;
+    public TableColumn colContact;
+    public TableColumn colAddress;
+    public TableColumn colNic;
 
     public void btnSearchOnAction(ActionEvent actionEvent) {
     }
