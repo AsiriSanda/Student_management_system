8da9f196119bb955a0fc66dd27eb8de983dc9337
AsiriSanda <sadakelum1234@gmail.com>
2022-09-20 09:48:41 +0530

    Student ui implement

--- src/controller/StudentController.java
+++ src/controller/StudentController.java
@@ -1,4 +1,22 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class StudentController {
+    public TextField txtId;
+    public TextField txtNIC;
+    public TextField txtAddress;
+
+    public void btnSearchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
 }
