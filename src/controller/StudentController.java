91f783399ded045b69db5359d379c658afa5b0ab
AsiriSanda <sadakelum1234@gmail.com>
2022-09-20 10:18:03 +0530

    Student table load

--- src/controller/StudentController.java
+++ src/controller/StudentController.java
@@ -1,9 +1,21 @@
 package controller;
 
+import db.DbConnection;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.student;
+import view.tm.studentTM;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class StudentController {
     public TextField txtId;
@@ -12,13 +24,62 @@ public class StudentController {
     public TextField txtEmail;
     public TextField txtName;
     public TextField txtContact;
-    public TableView tblStudent;
+    public TableView<studentTM> tblStudent;
     public TableColumn colId;
     public TableColumn colName;
     public TableColumn colEmail;
     public TableColumn colContact;
     public TableColumn colAddress;
     public TableColumn colNic;
+
+    public void initialize() {
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        colContact.setCellValueFactory(new PropertyValueFactory<>("contact"));
+        colAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
+        colNic.setCellValueFactory(new PropertyValueFactory<>("nic"));
+
+        loadAllStudents();
+
+        tblStudent.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                txtId.setText(newValue.getId());
+                txtName.setText(newValue.getName());
+                txtEmail.setText(newValue.getEmail());
+                txtContact.setText(newValue.getContact());
+                txtAddress.setText(newValue.getAddress());
+                txtNIC.setText(newValue.getNic());
+            }
+        });
+    }
+
+    private void loadAllStudents() {
+        ArrayList<student> students = new ArrayList<>();
+        ObservableList<studentTM> obList = FXCollections.observableArrayList();
+        try {
+            Connection connection = DbConnection.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM student");
+            ResultSet rst = preparedStatement.executeQuery();
+
+            while (rst.next()) {
+                students.add(new student(
+                        rst.getString(1),
+                        rst.getString(2),
+                        rst.getString(3),
+                        rst.getString(4),
+                        rst.getString(5),
+                        rst.getString(6))
+                );
+            }
+            for (student stu : students) {
+                obList.add(new studentTM(stu.getId(), stu.getName(), stu.getEmail(), stu.getContact(), stu.getAddress(), stu.getNic()));
+            }
+            tblStudent.setItems(obList);
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public void btnSearchOnAction(ActionEvent actionEvent) {
     }
